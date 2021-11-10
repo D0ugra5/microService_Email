3bd2f6aeaa57ac5a624855bc8aa7e514dc4c00bc
DevsJabinho <douglasmb87@gmail.com>
2021-11-09 23:43:50 -0300

    final

---
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,29 @@
+package com.ms.email.models;
+
+import com.ms.email.enums.StatusEmail;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+
+
+
+
+}
